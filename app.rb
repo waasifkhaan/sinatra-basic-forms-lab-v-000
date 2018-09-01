d747488da2fc71d95545a88718feb3c4bb765d71
Wasif Khan <waasif.khaan@gmail.com>
2018-09-01 20:18:17 +0000

    Done.

--- app.rb
+++ app.rb
@@ -6,8 +6,14 @@ class App < Sinatra::Base
     erb :index
   end 
   
-  get '/create_puppy' do
+  get '/new' do
     erb :create_puppy
+  end 
+  
+  post '/puppy' do 
+    @puppy = Puppy.new(params[:name],params[:breed],params[:age])
+   
+    erb :display_puppy
   end 
     
   
