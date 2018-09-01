26abc198579b05a9580811acb341f987b56ea4b4
Wasif Khan <waasif.khaan@gmail.com>
2018-09-01 20:09:15 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ class App < Sinatra::Base
     erb :create_puppy
   end 
   
-  post '/display_puppy'
+  post '/display_puppy' do 
     @name = params[:puppy_name]
     @breed = params[:puppy_breed]
     @age = params[:puppy_age]
